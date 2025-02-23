21ed2e625cf2efa07c64226e1b12a637224a621f
Ben Di Maggio <ben.dimaggio@thinkshout.com>
2025-02-23 22:40:03 +0100

    'Theme Panel' sounded stupid.

--- wp-content/themes/dark-academia/functions.php
+++ wp-content/themes/dark-academia/functions.php
@@ -86,7 +86,7 @@ function header_bg_button() {
 function theme_settings_page() {
 	?>
 		<div class="wrap">
-		<h1>Theme Panel</h1>
+		<h1>Theme Settings</h1>
 		<form method="post" action="options.php">
 			<?php
 				settings_fields('section');
@@ -99,13 +99,13 @@ function theme_settings_page() {
 }
 
 function add_theme_menu_item() {
-	add_menu_page( 'Theme Panel', 'Theme Panel', 'manage_options', 'theme-panel', 'theme_settings_page', null, 99 );
+	add_menu_page( 'Theme Settings', 'Theme Settings', 'manage_options', 'theme-panel', 'theme_settings_page', null, 99 );
 }
 
 add_action('admin_menu', 'add_theme_menu_item');
 
 function display_theme_panel_fields() {
-	add_settings_section('section', 'All Settings', null, 'theme-options');
+	add_settings_section('section', '', null, 'theme-options');
 
 	add_settings_field('header_bg', 'Header Background Image', 'header_bg_display', 'theme-options', 'section');
 	add_settings_field('header_bg_button', '', 'header_bg_button', 'theme-options', 'section');
